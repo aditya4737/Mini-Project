c6cb2906d4bd67e98cfa6d905e2f589bab4cbc45
Aditya <adityakhavate@gmail.com>
2024-08-06 21:06:14 +0530

    login sigin home and three option

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,34 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter , Routes, Route } from 'react-router-dom';
+
 import './App.css'
+import Signup from './pages/Signup'
+import Signin from './pages/Signin'
+import CaseForm from './pages/CaseForm'
+import CaseEinvoice from './pages/CaseEinvoice'
+import Lawyers from './pages/Lawyers'
+import About from './pages/About'
+import Chat from './pages/Chat';
+import Home from './pages/Home';
+import Header from './components/Header';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+    <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/About" element={<About/>}/>
+        <Route path="/Signin" element={<Signin/>}/>
+        <Route path="/Signup" element={<Signup/>}/>
+        <Route path="/CaseForm" element={<CaseForm/>}/>
+        <Route path="/CaseEinvoice" element={<CaseEinvoice/>}/>
+        <Route path="/Lawyers" element={<Lawyers/>}/>
+        <Route path="/Chat" element={<Chat/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -1,47 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 
-const Signup = () => {
+const Signin = () => {
   return (
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-4 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Register now!!
+            Login your Account
           </h2>
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <form action="#" method="POST" className="space-y-6">
-            <div>
-              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                First name
-              </label>
-              <div className="mt-2">
-                <input
-                  id="first_name"
-                  name="first_name"
-                  type="first_name"
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-
-            <div>
-              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                Last name
-              </label>
-              <div className="mt-2">
-                <input
-                  id="last_name"
-                  name="last_name"
-                  type="last_name"
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-
             <div>
               <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
                 Email address
@@ -77,36 +47,19 @@ const Signup = () => {
             </div>
 
             <div>
-              <div className="flex items-center justify-between">
-                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                  Confirm Password
-                </label>
-              </div>
-              <div className="mt-2">
-                <input
-                  id="confirm_password"
-                  name="confirm_password"
-                  type="passwconfirm_passwordord"
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-
-            <div>
               <button
                 type="submit"
                 className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
-                Register
+                Signin
               </button>
             </div>
           </form>
 
           <p className="mt-10 text-center text-sm text-gray-500">
-            Already registred?{' '}
-            <Link to={"/Signin"} className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-              Signin
+            New member?{' '}
+            <Link to={"/Signup"} className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+              Register
             </Link>
           </p>
         </div>
@@ -114,4 +67,5 @@ const Signup = () => {
   )
 }
 
-export default Signup
+export default Signin
+
